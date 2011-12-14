b65f2830e750c394530b2584c0f1812960e67450
Dylan Vassallo <dylanvassallo@gmail.com>
2011-12-14 15:22:48 -0800

    Comments and final UI tweaks

--- app.rb
+++ app.rb
@@ -5,12 +5,15 @@ require 'json'
 require 'savon'
 require 'sinatra/base'
 
+# Silence the output of third-party libraries
 HTTPI.log = false
 Savon.configure do |config|
 	config.log = false
 end
 
 class NASA
+	# This class implements the public method 
+	# ground_stations_json as required by the specification. 
 
 	def initialize
 		@soap_client = Savon::Client.new('http://sscweb.gsfc.nasa.gov/WS/ssc/2/SatelliteSituationCenterService?wsdl')
@@ -28,17 +31,22 @@ class NASA
 end
 
 class GroundStationApp < Sinatra::Base
+	# This class implements a Sinatra web application. 
 	
 	configure do
 		enable :logging
-		enable :static
+		enable :static # Serve static files from the /public directory
 	end
 
 	get '/' do
+		# No server-side processing or template rendering is necessary
+		# in the index. Simply redirect to the static page. 
 		redirect to('/index.html')
 	end
 	
 	get '/stations' do
+		# The interface's JavaScript code calls this endpoint to download 
+		# the ground station coordinates in JSON format. 
 		content_type :json
 		NASA.new.ground_stations_json
 	end
